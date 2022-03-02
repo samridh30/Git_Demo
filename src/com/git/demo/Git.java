df671a4629bf0ed97e002cef5c25ce8562792bea
samridh30 <97982455+samridh30@users.noreply.github.com>
2022-03-02 14:22:17 +0530

    Initial commit

---
+++ src/com/git/demo/Git.java
@@ -0,0 +1,25 @@
+package com.git.demo;
+
+public class Git {
+	 public static void main(String[] args) {
+
+		    int num = 40, i = 2;
+		    boolean flag = false;
+		    while (i <= num / 2) {
+		      if (num % i == 0) {
+		        flag = true;
+		        break;
+		      }
+
+		      ++i;
+		    }
+
+		    if (!flag)
+		      System.out.println(num + " is a prime number.");
+		    else
+		      System.out.println(num + " is not a prime number.");
+		  }
+		}
+
+
+
